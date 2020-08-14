cdb6ddabfb83a873109cbc85e1ed9db1d9bdd829
Nomanishaq <nomanishaq251@gmail.com>
2020-08-15 01:07:07 +0500

    update

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,42 +1,80 @@
 import React from 'react';
 import home from '../../img/home.png';
+import web from '../../img/webdevelopment.jpg';
+import design from '../../img/designing.jpg';
 import style from './Home.module.css';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import GitHubIcon from '@material-ui/icons/GitHub';
-import TwitterIcon from '@material-ui/icons/Twitter';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import InstagramIcon from '@material-ui/icons/Instagram';
-import {Grid ,Container} from '@material-ui/core';
-
+import { Grid, Container } from '@material-ui/core';
 
 export default function Home() {
- 
+  // social media links
+  const Link = (value) => {
+    if (value === 'facebook') {
+      window.open('https://www.facebook.com/nomanishaq2');
+    } else if (value === 'github') {
+      window.open('https://www.github.com/nomanishaq');
+    }
+    else if (value === 'linkedin') {
+      window.open('https://www.linkedin.com/in/nomanishaq/');
+    }
+    else if (value === 'instagram') {
+      window.open('https://www.instagram.com/noman_ishaq_/');
+    }
+  }
   return (
-    <div className={style.home}>
-    <Container ><Grid container spacing={3}>
-        <Grid item xs={12} sm={6}>
-        <img src={home} className={style.homeImg} alt="home" />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <div className={style.homeContent}>
-            <h1 className={style.heading}>I Am Noman Ishaq</h1>
-            <h2 className={style.title}>Full Stack Developer</h2>
-          </div>
-          <div className={style.socialIcon}>
-              <Grid item xs={12} sm={12}>
-                  <FacebookIcon className={style.icon}/>
-                  <GitHubIcon className={style.icon}/>
-                  <LinkedInIcon  className={style.icon}/>
-                  <TwitterIcon  className={style.icon}/>
-                  <InstagramIcon  className={style.icon}/>
+    <div className={style.container}>
 
-              </Grid>
-          </div>
-        </Grid>
+      {/* home section start*/}
+      <section className={style.home}>
+        <Container>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={6}>
+              <img src={home} className={style.homeImg} alt="home" />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <div className={style.homeContent}>
+                <h1 className={style.heading}>I Am Noman Ishaq</h1>
+                <h2 className={style.title}>Full Stack Developer</h2>
+              </div>
+              <div className={style.socialIcon}>
+                <Grid item xs={12} sm={12}>
+                  <FacebookIcon className={style.icon} onClick={() => Link('facebook')} />
+                  <GitHubIcon className={style.icon} onClick={() => Link('github')} />
+                  <LinkedInIcon className={style.icon} onClick={() => Link('linkedin')} />
+                  <InstagramIcon className={style.icon} onClick={() => Link('instagram')} />
+                </Grid>
+              </div>
+            </Grid>
+          </Grid>
+        </Container>
+      </section>
+      {/* home section end */}
+     
+     
+      <section className={style.skillSection}>
+         <Container>
+          <h1 className={style.skillsTittle}>Skills</h1>
 
-       
-      </Grid>
-      </Container>
+          <Grid container spacing={5}>
+                <Grid item xs={12} sm={6}>
+                 <div>
+                 <img src={web} alt=" " width="100%"/>
+                 <h2>Web Developer</h2>
+                 </div>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                <div>
+                <img src={design} alt=" " width="100%"/>
+                <h2>Web Designer</h2>
+                </div>
+                </Grid>
+          </Grid>
+         </Container>
+      </section>
+
     </div>
   );
 }
